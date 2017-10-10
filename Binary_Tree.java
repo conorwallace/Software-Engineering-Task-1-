80d6c164bd28a0e2411f93878086b5acc68fbbd2
conorwallace <32296355+conorwallace@users.noreply.github.com>
2017-10-10 13:40:50 +0100

    Added a Node class
    Added a class to allow object nodes to be created and begin to form a binary tree

--- Binary_Tree.java
+++ Binary_Tree.java
@@ -1,8 +1,23 @@
 
-public class Binary_Tree {
+		public class Binary_Tree<Key extends Comparable<Key>, Value> {
+		    private Node root;             // root of BST
 
-	public Binary_Tree() {
-		// TODO Auto-generated constructor stub
+		    /**
+		     * Private node class.
+		     */
+		    private class Node {
+		        private Key key;           // sorted by key
+		        private Value val;         // associated data
+		        private Node left, right;  // left and right subtrees
+		        private int N;             // number of nodes in subtree
+
+		        public Node(Key key, Value val, int N) {
+		            this.key = key;
+		            this.val = val;
+		            this.N = N;
+		        }
+		    }
+
 	}
 
-}
+
