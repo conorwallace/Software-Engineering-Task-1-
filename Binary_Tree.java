3f72d896607b8c4d43ddf47b82c7b867d8c28c11
conorwallace <32296355+conorwallace@users.noreply.github.com>
2017-10-12 13:38:47 +0100

    More Methods Added

--- Binary_Tree.java
+++ Binary_Tree.java
@@ -1,8 +1,6 @@
+import BST.Node;
 
-
-		
-
-		public class Binary_Tree<Key extends Comparable<Key>, Value> {
+public class Binary_Tree<Key extends Comparable<Key>, Value> {
 		    private Node root;             // root of BST
 
 		    /**
@@ -19,6 +17,45 @@
 		            this.val = val;
 		            this.N = N;
 		        }
+		     
+		    }
+		    
+		    public int size(){
+				return size(root);
+			}
+			
+			public int size(Node x){
+				if(x==null){
+					return 0;
+				}else{
+					return x.N;
+				}
+			}
+		    
+		    // method to enter data into the BST
+		    
+		    public void put(Key key, Value val) {
+		       
+		        root = put(root, key, val);
+		    }
+
+		    private Node put(Node x, Key key, Value val) {
+		    	
+		        if (x == null) return new Node(key, val, 1);
+		        int cmp = key.compareTo(x.key);
+		        if      (cmp < 0) x.left  = put(x.left,  key, val);
+		        else if (cmp > 0) x.right = put(x.right, key, val);
+		        else              x.val   = val;
+		        x.N = 1 + size(x.left) + size(x.right);
+		        return x;
+		    }
+		    
+		    
+		    
+		    // method to find the Lowest Common Ancestor
+		    public class lowestCommonAncestor
+		    {
+		    	
 		    }
 
 	}
