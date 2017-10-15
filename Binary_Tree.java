2b73d7ee42d958d2c8b8cc548bb879ceccff6598
conorwallace <32296355+conorwallace@users.noreply.github.com>
2017-10-15 12:08:30 +0100

    Edited and changed functions of the program

--- Binary_Tree.java
+++ Binary_Tree.java
@@ -1,94 +1,84 @@
-import BST.Node;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Binary_Tree<Key extends Comparable<Key>, Value> {
-		    private Node root;             // root of BST
+ class Node 
+{
+	int data;
+	Node left, right;
+	
+	Node(int value)
+	{
+		data = value;
+		left=right=null;
+	}
 
-		    /**
-		     * Private node class.
-		     */
-		    private class Node {
-		        private Key key;           // sorted by key
-		        private Value val;         // associated data
-		        private Node left, right;  // left and right subtrees
-		        private int N;             // number of nodes in subtree
+	
 
-		        public Node(Key key, Value val, int N) {
-		            this.key = key;
-		            this.val = val;
-		            this.N = N;
-		        }
-		     
-		    }
-		    
-		    public int size(){
-				return size(root);
-			}
-			
-			public int size(Node x){
-				if(x==null){
-					return 0;
-				}else{
-					return x.N;
-				}
-			}
-		    
-		    // method to enter data into the BST
-		    
-		    public void put(Key key, Value val) {
-		       
-		        root = put(root, key, val);
-		    }
-
-		    private Node put(Node x, Key key, Value val) {
-		    	
-		        if (x == null) return new Node(key, val, 1);
-		        int cmp = key.compareTo(x.key);
-		        if      (cmp < 0) x.left  = put(x.left,  key, val);
-		        else if (cmp > 0) x.right = put(x.right, key, val);
-		        else              x.val   = val;
-		        x.N = 1 + size(x.left) + size(x.right);
-		        return x;
-		    }
-		    
-		    
 		    
 		    // method to find the Lowest Common Ancestor
 		    public class lowestCommonAncestor
 		    {
 		    	Node root;
 		    	
-		    	boolean F1 = false;
-		    	boolean F2 = false;
+		    	private List <Integer> path1 = new ArrayList<>();
+		    	private List<Integer> path2 = new ArrayList<>();
 		    	
-		    	Node findLowestCommonAncestor(int n1 ,int n2)
+		    	int findLCA(int n1, int n2)
 		    	{
-		    		Node lac = findLCA(root,n1,n2);
-		    		
-		    		if (F1 && F2)
-		    		{
-		    			return lac;
-		    		}
-		    		
-		    		return lac;
+		    		path1.clear();
+		    		path2.clear();
+		    		return findLCA (root, n1, n2);
+		    	
 		    	}
 		    	
-		    	private Node findLCA(Node node, int n1, int n2)
+		    		
+		    	
+		    	private int findLCA(Node root,int n1 ,int n2)
 		    	{
-		    		if(node == null)
+		    		if (!findPath(root, n1, path1) || !findPath(root, n2, path2))
 		    		{
-		    			return null;
-		    			
+		    			return -1;
 		    		}
 		    		
-		    		if(this.node.val == n1)
+		    		int i;
+		    		for(i=0;i<path1.size() && i < path2.size();i++)
 		    		{
-		    			
+		    			if(!path1.get(i).equals(path2.get(i)))
+		    					{
+		    					break;
+		    					}
 		    		}
+		    	 return path1.get(i-1);
 		    	}
 		    	
-		    	
+		    	private boolean findPath(Node root, int n, List<Integer> path)
+		    	{
+		    		if(root==null)
+		    		{
+		    			return false;
+		    		}
+		    		
+		    		path.add(root.data);
+		    		
+		    		if(root.data == n)
+		    		{
+		    			return true;
+		    		}
+		    		if (root.left != null && findPath(root.left, n , path))
+		    		{
+		    			return true;
+		    		}
+		    		if(root.right != null && findPath(root.right, n ,path))
+		    		{
+		    			return true;
+		    		}
+		    		
+		    		path.remove(path.size()-1);
+		    		
+		    		return false;
+		    	}
+
 		    }
-
-	}
-
+}
+ 
 
